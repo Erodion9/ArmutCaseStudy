5ff6a27ef7f70c26c0ceab405005d81531e70b39
Erodion9 <denizmavi@sabanciuniv.edu>
2020-09-03 18:51:02 +0300

    Add Base ViewController and ViewModel classes

---
+++ Armut/Base/BaseViewController.swift
@@ -0,0 +1,11 @@
+//
+//  BaseViewController.swift
+//  Armut
+//
+//  Created by Deniz Mavi on 3.09.2020.
+//  Copyright © 2020 Deniz Mavi. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController { }
---
+++ Armut/Base/BaseViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  BaseViewModel.swift
+//  Armut
+//
+//  Created by Deniz Mavi on 3.09.2020.
+//  Copyright © 2020 Deniz Mavi. All rights reserved.
+//
+
+class BaseViewModel { }
