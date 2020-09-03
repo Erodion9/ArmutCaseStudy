bb225f02c58004d0b3c0cfe455bd5a8f82aecd43
Erodion9 <denizmavi@sabanciuniv.edu>
2020-09-03 18:47:20 +0300

    Initial Commit

---
+++ Armut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Armut
+//
+//  Created by Deniz Mavi on 3.09.2020.
+//  Copyright © 2020 Deniz Mavi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
